0167488c037c8593ddc699b72c3ab589eb8a0a02
Felix bachmair <felix.bachmair@gmail.com>
2020-01-07 20:15:54 +0100

    startet to using logging class

--- scripts/daemon_rfid_reader.py
+++ scripts/daemon_rfid_reader.py
@@ -2,8 +2,20 @@
 
 import subprocess
 import os, time
+import logging
 from Reader import Reader
 
+# create and configure main logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+# create console handler with a higher log level
+handler = logging.StreamHandler()
+handler.setLevel(logging.INFO)#DEBUG)
+# create formatter and add it to the handler
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+# add the handler to the logger
+logger.addHandler(handler)
 reader = Reader()
 
 # get absolute path of this script
@@ -13,23 +25,35 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 same_id_delay = 0
 previous_id = ""
 previous_time = time.time()
+logger.info('Dir_path: {}'.format( dir_path))
 
+logger.info('Ready to read a card')
 while True:
         # reading the card id
         # NOTE: it's been reported that KKMOON Reader might need the following line altered.
         # Instead of:
         # cardid = reader.reader.readCard()
         # change the line to:
-        # cardid = reader.readCard()
+        #cardid = reader.readCard()
         # See here for (German ;) details:
         # https://github.com/MiczFlor/RPi-Jukebox-RFID/issues/551
         cardid = reader.reader.readCard()
         try:
-            # start the player script and pass on the cardid (but only if new card or otherwise "same_id_delay" seconds have passed)
-            if cardid != None and (cardid != previous_id or (time.time()-previous_time) >= same_id_delay):
-                subprocess.call([dir_path + '/rfid_trigger_play.sh --cardid=' + cardid], shell=True)
-                previous_id = cardid
-                previous_time = time.time()
+            # start the player script and pass on the cardid
+            if cardid != None:
+                dt = time.time()-previous_time
+                if (cardid != previous_id or dt>= same_id_delay):
+                    logger.info('cardid: {}'.format(cardid))
+                    subprocess.call([dir_path + '/rfid_trigger_play.sh --cardid=' + cardid], shell=True)
+                    previous_id = cardid
+                    previous_time = time.time()
+                else:
+                    logger.info('ignoring card {cardid}, {dt} < {same_id_delay}'.format(
+                                cardid=cardid,
+                                dt=dt,
+                                same_id_delay=same_id_delay))
+            else:
+                logger.debug('no valid card id found: {}'.format(cardid))
 
         except OSError as e:
-            print "Execution failed:"
+            logger.exception("Execution failed: {}".format(e))
